90f09525734c3e80e7569f9cc5778dbf6f4118c9
Vitali Stsepaniuk <vstepanuyk@gmail.com>
2020-11-09 00:45:25 +0100

    new features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { snakeCase } from 'lodash'
-
 import { useScript } from './lib/hooks'
 
 const SUBSRIPTIONS_QUERY = gql`
@@ -10,6 +8,7 @@ query {
   items: websitePlans {
     _id
     name
+    alias
     description
     price {
       value
@@ -38,11 +37,11 @@ query {
 }
 `
 
-function App({ meta }) {
+function App({ meta, isLoggedIn, buttonStyles }) {
   const { data, loading, error } = useQuery(SUBSRIPTIONS_QUERY)
 
   const clientId = data?.settings?.clientId
-  const status = useScript(clientId ? `https://www.paypal.com/sdk/js?client-id=${encodeURIComponent(clientId)}&vault=true&intent=subscription` : null)
+  const status = useScript(clientId && isLoggedIn ? `https://www.paypal.com/sdk/js?client-id=${encodeURIComponent(clientId)}&vault=true&intent=subscription` : null)
 
   if (error) {
     return (
@@ -70,27 +69,24 @@ function App({ meta }) {
     <div className="subscriptions container">
       <div className="row">
       {(data?.items ?? []).map((item, index) => (
-        <div className={`d-flex align-items-stretch col-sm subscription subscription-${index} subscription-${snakeCase(item.name)}`} key={item._id}>
+        <div className={`d-flex align-items-stretch col-sm subscription subscription-${index} subscription-${item.alias}`} key={item._id}>
           <div className="card">
             <h5 className="card-header">{item.name}</h5>
             <div className="card-body" dangerouslySetInnerHTML={{ __html: item.description }} />
-            {status === 'ready' && (
+            {status === 'ready' && item.price?.value > 0 &&  (
               <div className="paypal-buttons">
                 <div className="text-center">
                   <PayPalButton
                     createSubscription={(data, actions) => {
                       return actions.subscription.create({ 
                         plan_id: item.paypalId,
-                        custom_id: JSON.stringify({
-                          ...meta,
-                          sid: item._id
-                        })
+                        custom_id: JSON.stringify(meta.concat(item._id))
                       })
                     }}
-                    style ={{
+                    style ={buttonStyles ?? {
                       color: 'blue',
                       shape: 'pill',
-                      height: 30,
+                      height: 40
                     }}
                     onApprove={(data, actions) => {
                       return actions.subscription.get().then(function(details) {
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import createApolloClient from './lib/apollo'
 import { ApolloProvider } from '@apollo/client';
 
-export function init({ apiUrl, apiKey, element, meta }) {
+export function init({ apiUrl, apiKey, element, meta, isLoggedIn = false, buttonStyles } = {}) {
   ReactDOM.render(
     <ApolloProvider client={createApolloClient(apiUrl, apiKey)}>
-      <App meta={meta ?? {}} />
+      <App meta={meta ?? {}} isLoggedIn={isLoggedIn} buttonStyles={buttonStyles} />
     </ApolloProvider>,
     element ?? document.getElementById('subscriptions')
   );
